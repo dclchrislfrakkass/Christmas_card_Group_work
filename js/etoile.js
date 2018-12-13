bbecf94224312312235fed181699902e989b38fb
dclcoconuts <dcl.corinnem@18pixel.fr>
2018-12-13 13:35:13 +0100

    etoile filante en aleatoire

---
+++ js/etoile.js
@@ -0,0 +1,13 @@
+
+function randomPosition(){
+    var x = Math.floor(Math.random() * 50);
+    var positionX = x+"%";
+    var y = Math.floor(Math.random() * 20);
+    var positionY = y+"%";
+    document.getElementById("night_shooting_star").style.left = positionX ;
+    document.getElementById("night_shooting_star").style.top = positionY ;
+
+}
+var timer=setInterval("randomPosition()" , 9000);
+
+//    document.getElementById("night_shooting_star").style.left = "200px";
